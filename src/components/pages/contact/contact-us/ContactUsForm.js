a76e48f6e6d4c7137af6d09dc28c0b043df3795a
marek <marekreid@gmail.com>
2021-07-02 06:01:16 +0200

    fixed contact form error

--- src/components/pages/contact/contact-us/ContactUsForm.js
+++ src/components/pages/contact/contact-us/ContactUsForm.js
@@ -67,20 +67,7 @@ const ContactUsForm = () => {
     !inputs.fName
       ? setError(error => ({ ...error, fName: true }))
       : setError(error => ({ ...error, fName: false }))
-    inputs.sName.length === 0
-      ? setError(error => ({ ...error, sName: true }))
-      : setError(error => ({ ...error, sName: false }))
-    inputs.email.length === 0 ||
-    !inputs.email.includes(".") ||
-    !inputs.email.includes("@")
-      ? setError(error => ({ ...error, email: true }))
-      : setError(error => ({ ...error, email: false }))
-    inputs.number.length === 0
-      ? setError(error => ({ ...error, number: true }))
-      : setError(error => ({ ...error, number: false }))
-    inputs.message.length === 0
-      ? setError(error => ({ ...error, message: true }))
-      : setError(error => ({ ...error, message: false }))
+  
   }
 
   const encode = data => {
