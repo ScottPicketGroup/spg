ba9eec823366191e6c07ee7947944dddc093d549
marek <marekreid@gmail.com>
2021-08-11 18:19:46 +0200

    new form

--- src/components/pages/contact/ContactForm/ContactFormNew.js
+++ src/components/pages/contact/ContactForm/ContactFormNew.js
@@ -47,7 +47,7 @@ const encode = (data) => {
   const handleChange = e => setInputs({ [e.target.name]: e.target.value });
   console.log(inputs)
   return (
-<form onSubmit={handleSubmit}>
+<form onSubmit={handleSubmit} name="contact" netlify>
           <p>
             <label>
               Your Name: <input type="text" name="name" value={inputs.name} onChange={handleChange} />
