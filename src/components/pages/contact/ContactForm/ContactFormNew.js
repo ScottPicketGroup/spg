d0f49dfeda6cada457709d93522817036e0319fc
marek <marekreid@gmail.com>
2021-08-12 00:15:15 +0200

    fixed all of the forms I think

--- src/components/pages/contact/ContactForm/ContactFormNew.js
+++ src/components/pages/contact/ContactForm/ContactFormNew.js
@@ -40,7 +40,7 @@ export default class FormNew extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1>Contact</h1>
+              <h1>Contact Us</h1>
               <form
                 name="contact-new"
                 method="post"
