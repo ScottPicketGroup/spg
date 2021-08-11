83eaf04f413bdb73f41da13744cbcf87e5512abd
marek <marekreid@gmail.com>
2021-08-11 18:14:22 +0200

    new form

--- src/components/pages/contact/ContactForm/ContactFormNew.js
+++ src/components/pages/contact/ContactForm/ContactFormNew.js
@@ -13,202 +13,62 @@ const ContactFormNew = () => {
   const [error, setError] = useState({
     fName: false
   })
-  const [inputs, setInputs] = useState({
-    fName: "",
-    sName: "",
-    email: "",
-    number: "",
-    message: "",
-    messageLength: 0,
-    newsletter: false,
-  })
+
 
   const [form, setForm] = useState(true)
   const [thankyou, setThankyou] = useState(false)
-  const handleChange = e => {
-    setInputs(inputs => ({ ...inputs, [e.target.name]: e.target.value }))
-    
- 
-    !inputs.email.includes(".") || !inputs.email.includes("@") 
-      ? setError(error => ({ ...error, email: true}) )
-      : setError(error => ({ ...error, email: false}) )
 
-  }
 
 const encode = (data) => {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
         .join("&");
   }
+ 
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
   
+ 
   const handleSubmit = e => {
-   const timestamp = Date.now()
-   
-   if((   inputs.fName 
-    && inputs.sName 
-    && inputs.email 
-    && inputs.email.includes(".")) || inputs.email.includes("@"))   
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...inputs })
     })
-      .then(
-     
-          setThankyou(true)
-     
-
-      )
+      .then(() => alert("Success!"))
       .catch(error => alert(error));
-    
-      if ( inputs.newsletter === true && (inputs.email && inputs.email.includes(".")) || inputs.email.includes("@")) {
-        var myHeaders = new Headers()
-        myHeaders.append(
-          "Authorization",
-          "Bearer 25183d2e-1266-4207-a9d3-a5d9422d94b0"
-        )
-        myHeaders.append("Timestamp", {timestamp})
-        myHeaders.append("Content-Type", "application/json")
-  
-        var raw = JSON.stringify({
-          data: {
-            email: inputs.email,
-          },
-        })
-  
-        var requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          redirect: "follow",
-        }
-  
-        fetch("https://api.sproutsend.com/contacts?", requestOptions)
-          .then(response => response.json())
-          .then(result => console.log('result', result))
-       
-  
-          .catch(error => console.log("error", error))
-      } 
 
-      e.preventDefault()
-      
-  }
+    e.preventDefault();
+  };
+
+  const handleChange = e => setInputs({ [e.target.name]: e.target.value });
+  console.log(inputs)
   return (
-    <ContactUsFormContainer>
-        {!thankyou ? (
-            <>
- <SignUp 
- name="contact" 
- method="post" 
- data-netlify="true" 
- data-netlify-honeypot="bot-field" 
- >
-
-<input type="hidden" name="bot-field" />
-<input type="hidden" name="form-name" value="contact" />
-
- <ContactDetailsContainer>
-     <ContactFormRow>
-     <InputContainer>
-     <Label>NAME</Label>
-     <Input
-       placeholder="Please enter your first name"
-       name="fName"
-       type="text"
-       value={inputs.fName}
-       onChange={handleChange}
-       err={error.fName}
-     />
-     
-   </InputContainer>
-   <InputContainer>
-     <Label>SURNAME</Label>
-     <Input
-       placeholder="Please enter your surname"
-       name="sName"
-       type="text"
-       value={inputs.sName}
-       onChange={handleChange}
-       err={error.sName}
-     />
-    
-   </InputContainer>
-     </ContactFormRow>
-     <ContactFormRow>
-     <InputContainer>
-     <Label>EMAIL ADDRESS</Label>
-     <Input
-       placeholder="Please enter your email address"
-       name="email"
-       type="text"
-       value={inputs.email}
-       onChange={handleChange}
-       err={error.email}
-     />
-    
-   </InputContainer>
-   <InputContainer>
-     <Label>CONTACT NUMBER</Label>
-     <Input
-       placeholder="Please enter your contact number"
-       name="number"
-       type="text"
-       value={inputs.number}
-       onChange={handleChange}
-       err={error.number}
-     />
-    
-   </InputContainer>
-     </ContactFormRow>
-   
-
- <InputContainer>
-     <Label>MESSAGE</Label>
-     <InputMessage
-       placeholder="Please enter your enquiry (1000 characters remaining)"
-       name="message"
-       type="text"
-       value={inputs.message}
-       onChange={handleChange}
-       err={error.message}
-       scroll="disable"
-     />
-     {inputs.message.length >=1 ? (
-      <p  style={{ marginTop: `1rem`, fontSize: `1rem`}}>
-     {1000 - inputs.message.length} characters remaining
-    </p>
-     ): error.message ? (<span></span>) : null}
-  
-   </InputContainer>
-   <TixboxContainer >
-     <div onClick={() => setInputs(inputs => ({ ...inputs, newsletter: true })) }>
-     
-    <CheckBox  />
-    </div>
-       <Label bc2 style={{width: `90%`}}>I would like to receive communications about Scott Pickett Group services, events and matters of relevant interest.</Label>
-   </TixboxContainer>
-   <SignUpSubmit onClick={handleSubmit} err={error.email} type="submit">
-   SUBMIT
- </SignUpSubmit> 
- </ContactDetailsContainer>
+<form onSubmit={handleSubmit}>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" value={inputs.name} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" value={inputs.email} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" value={inputs.message} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
 
 
-
-</SignUp>
-
- </>
-        ) :
-        (
-            <>
-            <Label thankyou> Thank you for submitting your enquirey.</Label>
-            <Label thankyou> A member of our staff will be in contact shortly.</Label>
-            </>
-        )
-        }
-     
-    
-    </ContactUsFormContainer>
   )
 }
 
