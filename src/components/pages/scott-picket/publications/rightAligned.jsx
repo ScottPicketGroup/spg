7eb7ac4ec5ebeda97b0c2b4b765936e6ddc3b79c
marek <marekreid@gmail.com>
2021-08-11 14:35:18 +0200

    added publications images

--- src/components/pages/scott-picket/publications/rightAligned.jsx
+++ src/components/pages/scott-picket/publications/rightAligned.jsx
@@ -1,44 +1,40 @@
-import React from "react";
-import { useTheme } from "styled-components";
+import React from "react"
+import { useTheme } from "styled-components"
 
-import {
+import { Grid, Item } from "./styled-components"
+import { BC3, Header2} from "../../../global/fontStyles"
+import { StaticImage } from "gatsby-plugin-image"
 
-  Grid,
-  Item,
+const LeftAligned = () => {
+  const theme = useTheme()
 
-} from "./styled-components";
-import { BC3, BC2, Header2, Header3 } from "../../../global/fontStyles";
-
-import PublicationImage from "./publicationImage";
-
-
-import spBook from '../../../../images/publications/sp-book.jpg'
-const LeftAligned = ({ data, images }) => {
-  const theme = useTheme();
-  
   return theme ? (
     <Grid cols="2">
-    
-     <Item>
-     <Header2 marginBottom="1rem" marginTop="-5rem"  theme={theme}>Scott Pickett: A Cook’s Story</Header2>
-       {/* <Header3 marginBottom="1rem" theme={theme}>Sweet. Sour. Bitter. Salty. Spicy. Umami. Temperature. Texture.</Header3> */}
+      <Item>
+        <Header2 marginBottom="1rem" marginTop="-5rem" theme={theme}>
+          Scott Pickett: A Cook’s Story
+        </Header2>
 
-      
-           <BC3 light >A Cook’s Story tells of my life combining a passion for food with the art and craft of cooking. It’s an open account of my dramas in the kitchen, the roller-coaster of opening a restaurant, the demands of opening more, and the pleasures and challenges of competition and success</BC3>
-     
-      
-      
-      <BC3 light>Available for purchase in local bookstores.</BC3>
-       
-     </Item>
-     <Item>
-      <PublicationImage src={spBook} />
-       
-     </Item>
-   </Grid>
+        <BC3 light>
+          A Cook’s Story tells of my life combining a passion for food with the
+          art and craft of cooking. It’s an open account of my dramas in the
+          kitchen, the roller-coaster of opening a restaurant, the demands of
+          opening more, and the pleasures and challenges of competition and
+          success
+        </BC3>
+
+        <BC3 light>Available for purchase in local bookstores.</BC3>
+      </Item>
+      <Item>
+        <StaticImage
+          src="../../../../images/publications/sp-book.jpg"
+          alt="A cooks story"
+        />
+      </Item>
+    </Grid>
   ) : (
     <div></div>
-  );
-};
+  )
+}
 
-export default LeftAligned;
+export default LeftAligned
