0bc59c62ead8d82d785aa2830488082fa798b506
marek <marekreid@gmail.com>
2021-08-11 18:33:07 +0200

    new form

--- src/components/pages/landing/mobileLanding.jsx
+++ src/components/pages/landing/mobileLanding.jsx
@@ -37,7 +37,7 @@ const captions = [
 const MobileLanding = ({ pageProps }) => {
   const [chancery, setChancery] = useState(false)
 
-  const [, setChancery] = useState(false)
+
   
   const data = useStaticQuery(graphql`
     {
