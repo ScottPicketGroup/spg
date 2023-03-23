910a3a43dfa794adea1c69fc555eb96b83898995
Marek <marekreid@gmail.com>
2023-03-23 18:45:22 +0100

    finished product page

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,7 +47,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
     products.data.allContentfulPantryProduct.edges.forEach(({ node }) => {
         products.data.allContentfulPantryProduct.edges.forEach(({ node }) => {
-            const pathToUse = node.productName.toLowerCase().replace(/\s/g, '+')
+            const pathToUse = node.proudctName && node.productName.toLowerCase().replace(/\s/g, '+')
             const path = pathToUse && `/${pathToUse}`
             createPage({
               path,
