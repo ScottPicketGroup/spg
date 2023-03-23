d95457127b48656055d7ab390fa891a6104e2840
Marek <marekreid@gmail.com>
2023-03-23 09:22:55 +0100

    page up nothing there yet

--- src/pages/pantry.js
+++ src/pages/pantry.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Seo from "../components/seo";
+import { useTheme } from "styled-components";
 
-const SpPantry = () => {
-  return (
-    <div>SpPantry</div>
-  )
-}
 
-export default SpPantry
+
+const Pantry = (props) => {
+  const theme = useTheme();
+
+  return theme ? (
+    <div theme={theme}>
+      <Seo title="Scott Pickett Pantry" />
+   
+  SP Pantry1
+    </div>
+  ) : (
+    <div></div>
+  );
+};
+
+export default Pantry;
