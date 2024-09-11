643e29de4ca9d7d413baf6b64057f635d58febb0
Lmarcho-NeoSolax <your_email@example.com>
2024-09-11 16:46:54 +0530

    Scott Pickett request for new page

--- src/pages/efcmembers.js
+++ src/pages/efcmembers.js
@@ -1,27 +1,28 @@
-// src/pages/efcmembers.js
-import React from "react";
+import React, { useEffect } from "react";
 
 const EFCMembersPage = () => {
+    useEffect(() => {
+        // Dynamically inject the form script once the component has mounted
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "https://forms.contacta.io/66dfa05424058e9553ea7394.js";
+        script.async = true;
+        document.getElementsByTagName("head")[0].appendChild(script);
+
+        return () => {
+            // Clean up script if the component unmounts
+            if (script) {
+                script.remove();
+            }
+        };
+    }, []);
+
     return (
         <div>
             <h1>EFC Members Opt-In Form</h1>
             <p>Please fill out the form below to opt-in.</p>
-
             {/* Step 1: Div to hold the form */}
             <div id="CONTACTA_66dfa05424058e9553ea7394"></div>
-
-            {/* Step 2: Script to load the form dynamically */}
-            <script
-                type="text/javascript"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.src = "https://forms.contacta.io/66dfa05424058e9553ea7394.js";
-            document.getElementsByTagName("head")[0].appendChild(script);
-          `,
-                }}
-            />
         </div>
     );
 };
