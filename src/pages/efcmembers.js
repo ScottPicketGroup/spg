c7c480fbaa0302893017b2866cfdc501bcf306b5
Lmarcho-NeoSolax <your_email@example.com>
2024-09-12 08:37:46 +0530

    new changes for form

--- src/pages/efcmembers.js
+++ src/pages/efcmembers.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import '../../static/css/efcmembers.css'; // Import custom CSS for styling
+import logo from '../images/ScottPickettGroupLogo_Primary_PMS.svg'; // Adjust the path to your actual image file
 
 const EFCMembersPage = () => {
     useEffect(() => {
-        // Dynamically inject the form script once the component has mounted
+        // Dynamically inject the form script after the component mounts
         const script = document.createElement("script");
         script.type = "text/javascript";
         script.src = "https://forms.contacta.io/66dfa05424058e9553ea7394.js";
@@ -18,11 +20,14 @@ const EFCMembersPage = () => {
     }, []);
 
     return (
-        <div>
-            <h1>EFC Members Opt-In Form</h1>
-            <p>Please fill out the form below to opt-in.</p>
-            {/* Step 1: Div to hold the form */}
-            <div id="CONTACTA_66dfa05424058e9553ea7394"></div>
+        <div className="efc-members-container">
+            <div className="form-header">
+                {/* Add the Scott Pickett Group Logo */}
+                <img src={logo} alt="Scott Pickett Group Logo" className="logo" />
+            </div>
+
+            {/* Div to hold the dynamically loaded form */}
+            <div id="CONTACTA_66dfa05424058e9553ea7394" className="form-container"></div>
         </div>
     );
 };
